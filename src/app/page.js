ee2d5150a0d7683ea5c2ff16dc587af56fc99f0e
Agamjot Singh <agam@Agamjots-MacBook-Air.local>
2023-09-26 15:58:41 -0700

    sending final

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,38 @@
-import Image from 'next/image'
-import Header from '@/components/Header'
-import CountryList from '@/components/backend/CountryList'
+"use client"
+
+import Header from '@/components/Header';
+import { useState, useEffect } from 'react';
 
 export default function Home() {
+  const [info, setInfo] = useState([]);
+
+  useEffect(() => {
+    fetch('https://restcountries.com/v3.1/all')
+      .then((response) => response.json())
+      .then((data) => setInfo(data));
+      console.log(info)
+  }, []);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-[#f5f8f1]">
       <Header />
-      <CountryList />
+      <div className='returnValues'>
+        <ul>
+          {info.map((country, index) => (
+            <li  key={index}>
+              <h2>COUNTRY NAME: {country.name.common} CAPITOL: {country.capital} REGION: {country.region} </h2>
+              {country.flags && (
+                <img
+                  src={country.flags.png}
+                  width={50}
+                  height={60}
+                />
+              )}
+            </li>
+
+          ))}
+        </ul>
+      </div>
     </main>
-  )
+  );
 }
